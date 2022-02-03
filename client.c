de64d002552e840266245769c21dfb759e53c33b
Jack Sellars <jsellars@enee.com.au>
2022-02-03 20:26:44 +1030

    pushing a working build

--- client.c
+++ client.c
@@ -6,8 +6,43 @@
 /*   By: jsellars <jsellars@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 16:54:40 by jsellars          #+#    #+#             */
-/*   Updated: 2022/02/02 16:54:43 by jsellars         ###   ########.fr       */
+/*   Updated: 2022/02/03 17:07:17 by jsellars         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include<signal.h>
+#include"libft/libft.h"
 
+static void	send(int pid, char *str)
+{
+	int		i;
+	char	c;
+
+	while (*str)
+	{
+		i = 8;
+		c = *str++;
+		while (i--)
+		{
+			if (c >> i & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(100);
+		}
+	}
+	i = 8;
+	while (i--)
+	{
+		kill(pid, SIGUSR1);
+		usleep(100);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac != 3)
+		ft_putstr_fd("ac no 3\n", 1);
+	send(ft_atoi(av[1]), av[2]);
+	return (0);
+}
--- server.c
+++ server.c
@@ -6,57 +6,46 @@
 /*   By: jsellars <jsellars@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 16:54:29 by jsellars          #+#    #+#             */
-/*   Updated: 2022/02/02 17:37:55 by jsellars         ###   ########.fr       */
+/*   Updated: 2022/02/03 19:54:02 by jsellars         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <signal.h>
 
-static int	current_bit = 0;
-static int	current_byte = 0;
-static char	byte = 0;
-static char	str[100];
-
 void	handler(int sig)
 {
-	if (sig == SIGUSR2)
-		byte &= ~(128 >> current_bit);
-	if (sig == SIGUSR1)
-		byte |= ~(128 >> current_bit);
-	current_bit++;
-	if (current_bit > 7)
+	static int				i;
+	static unsigned char	c;
+
+	i = 0;
+	c = 0;
+	c |= (sig == SIGUSR2);
+	if (++i == 8)
 	{
-		current_bit = 0;
-		str[current_byte] = byte;
-		byte = 0;
-		if (current_byte < 99)
-			current_byte++;		
+		i = 0;
+		if (!c)
+			return ;
+		ft_putchar_fd(c, 1);
+		c = 0;
 	}
+	else
+		c <<= 1;
 }
 
 int	main(void)
 {
-	int	pid = getpid();
+	struct sigaction	sa;
+	int					pid;
+
+	pid = getpid();
+	sa.sa_flags = SA_RESTART;
+	sa.sa_handler = &handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	ft_putnbr_fd(pid, 1);
 	ft_putchar_fd('\n', 1);
-	
-	signal(SIGUSR1, handler);
-	signal(SIGUSR2, handler);
-
-	kill(SIGUSR1, pid);
-	kill(SIGUSR2, pid);
-	kill(SIGUSR1, pid);
-	kill(SIGUSR2, pid);
-	kill(SIGUSR1, pid);
-	kill(SIGUSR1, pid);
-	kill(SIGUSR1, pid);
-	kill(SIGUSR1, pid);
-	
-	ft_putchar_fd(str[0], 1);
-	/*
-	 *while (1)
-	 *        pause();
-	 *return (0);
-	 */
+	while (1)
+		pause();
+	return (0);
 }
